fbf2c3aef6dfec0173783cf876d630c6e4f9e815
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-06-13 02:33:07 -0700

    fix number idk

--- 0x03-python-data_structures/13-main.c
+++ 0x03-python-data_structures/13-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+
 /**
  * main - check the code for
  *
@@ -12,7 +13,15 @@ int main(void)
 
     head = NULL;
     add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 17);
+    add_nodeint_end(&head, 972);
+    add_nodeint_end(&head, 50);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 50);
+    add_nodeint_end(&head, 972);
+    add_nodeint_end(&head, 17);
+    add_nodeint_end(&head, 1);
     print_listint(head);
 
     if (is_palindrome(&head) == 1)
