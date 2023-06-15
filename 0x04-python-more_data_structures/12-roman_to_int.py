89a8513b3ae6c4932605a6704d84fe966b9c0fa3
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-06-14 18:32:45 -0700

    a fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    if not isinstance(roman_string, str):
+        return 0
+    roman_values = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+    summ = 0
+    prv = 0
 
+    for i in reversed(roman_string):
+        value = roman_values.get(i, 0)
+        if value >= prv:
+            summ += value
+        else:
+            summ -= value
+        prv = value
+    return summ
