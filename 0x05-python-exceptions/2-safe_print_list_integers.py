5f0100ae6cc03c15bff31dc08f027ac17df12e66
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-06-26 17:35:33 -0700

    a small fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    L = []
-    count = 0
+    sum = 0
     for i in range(x):
         try:
-            if isinstance(my_list[i], int):
-                L.append(my_list[i])
+            print("{:d}".format(my_list[i]), end="")
+            sum += 1
         except (ValueError, TypeError):
             continue
-    for i in L:
-        print("{:d}".format(i), end="")
-        count += 1
     print()
-    return count
+    return sum
