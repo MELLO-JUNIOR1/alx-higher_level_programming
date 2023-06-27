a04bbac1bb9ca0dc5b0932281cdf25b403280e91
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-06-26 17:33:03 -0700

    safe print list integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    L = []
+    count = 0
+    for i in range(x):
+        try:
+            if isinstance(my_list[i], int):
+                L.append(my_list[i])
+        except (ValueError, TypeError):
+            continue
+    for i in L:
+        print("{:d}".format(i), end="")
+        count += 1
+    print()
+    return count
