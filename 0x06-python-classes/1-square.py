27d14af6d5dccdae2357d42cb86bc5d58ba98bf9
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-06-29 18:15:07 -0700

    I'm pushing all the files cuz bro m tired and i wanna get some sleep nad ...

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Completing the class Square"""
+
+
+class Square:
+    """Class Square with Private instance attribute: size"""
+    __size = None
+
+    def __init__(self, size):
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Completing the class Square"""
+
+
+class Square:
+    """Class Square with Private instance attribute: size
+    The method __init__ contains exceptions for when size
+    is not an int and when it's less than zero.
+    The initialisation of size to zero will serve it's pu
+    -rpose when the size is not given"""
+    __size = None
+
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
