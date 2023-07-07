0684298f9c81980f1aff300f0bf8ada17867e1fa
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-07-07 14:56:53 -0700

    wllh ma3reft

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,22 +1,36 @@
 #!/usr/bin/python3
-def matrix_divided(matrix, div):
-    """setting tests"""
-    if (not isinstance(matrix, list) or matrix == [] or
-            not all(isinstance(row, list) for row in matrix) or
-            not all((isinstance(ele, int) or isinstance(ele, float))
-                    for ele in [num for row in matrix for num in row])):
-        raise TypeError("matrix must be a matrix (list of lists) of "
-                        "integers/floats")
-    size = len(matrix[0])
-    if not all(len(row) == size for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
+"""matrix_devided function"""
 
+
+def matrix_divided(matrix, div):
+    """
+    Function that performs devision of a matrix.
+
+    Returns :
+        new_matrix the matrix devided by dev
+
+    Raises :
+        TypeError in case the elements are not int nor float
+        TypeError in case the rows of the matrix are not the same size
+        TypeError in case div is not int nor float
+        ZeroDivisionError in case the div is equals to zero
+    """
+    new_matrix = []
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if not isinstance(matrix[i][j], (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) "
+                                "of integers/floats")
+        if i < len(matrix) - 1 and len(matrix[i]) != len(matrix[i + 1]):
+            raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
-    new = [[round(num / div, 2) for num in row] for row in matrix]
-
-    return new
+    for i in range(len(matrix)):
+        tmp = []
+        for j in range(len(matrix[i])):
+            division = round(matrix[i][j] / div, 2)
+            tmp.append(division)
+        new_matrix.append(tmp)
+    return new_matrix
