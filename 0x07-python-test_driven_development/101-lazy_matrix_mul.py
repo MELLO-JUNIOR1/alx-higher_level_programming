0a7b54e14ae83aa6ca355f98cf6d2a9075983a90
HamzaBouzakrya <115469329+MELLO-JUNIOR1@users.noreply.github.com>
2023-07-20 11:48:12 +0100

    Delete 0x07-python-test_driven_development directory

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""funtion that uses numpy"""
-import numpy as nmp
-
-
-def lazy_matrix_mul(m_a, m_b):
-    """ * of two matrix"""
-    return (nmp.matmul(m_a, m_b))
--- 0x07-python-test_driven_development/README.md
+++
@@ -1,1 +0,0 @@
-# python-test_driven_development
