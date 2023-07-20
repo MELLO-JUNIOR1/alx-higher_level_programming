69f3ef9fe05a4bdd48f9543c0a3b2967fbc287c4
HamzaBouzakrya <115469329+MELLO-JUNIOR1@users.noreply.github.com>
2023-07-20 11:48:02 +0100

    Delete 0x08-python-more_classes directory

--- 0x08-python-more_classes/0-rectangle.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""0x08. Python - More Classes and Objects"""
-
-
-class Rectangle:
-    """The class Rectangle"""
-    pass
--- 0x08-python-more_classes/README.md
+++
@@ -1,4 +0,0 @@
-# 0x08. Python - More Classes and Objects
-~~~~
-0x08. Python - More Classes and Objects first readme file
-~~~~
