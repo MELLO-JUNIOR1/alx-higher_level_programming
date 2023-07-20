3e8e726af9778b08fac21aacf779b8920c2922c6
HamzaBouzakrya <115469329+MELLO-JUNIOR1@users.noreply.github.com>
2023-07-20 11:47:49 +0100

    Delete 0x09-python-everything_is_object directory

--- 0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def magic_string(L=[]):
-    L.append("BestSchool")
-    return ", ".join(L)
--- 0x09-python-everything_is_object/README.md
+++
@@ -1,4 +0,0 @@
-# 0x09-python-everything_is_object
-~~~~
-0x09-python-everything_is_object
-~~~~
